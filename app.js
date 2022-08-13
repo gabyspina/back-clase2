ba6deb210e41be3e73d93eda0536f058ce64c057
gabyspina <gabyspina@gmail.com>
2022-08-13 09:42:22 -0300

    comenzamos a manipular el DOM

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-import { questionArray } from "./question.js";
- import { Encuesta } from "./models/encuesta.js";
+   import { questionArray } from "./question.js";
+   import { Encuesta } from "./models/encuesta.js";
+   import { UI } from "./models/UI.js";
+
 
  const main = () => {
     const encuesta = new Encuesta(questionArray);
+    const ui = new UI();
+
+      ui.showQuestion('pregunta'); 
+
     console.log( 'consulta', encuesta)
  }
 main()
---
+++ models/UI.js
@@ -0,0 +1,7 @@
+export class UI {
+    showQuestion(question) {
+        const questionTittle  = document.getElementById('question');
+        questionTittle.innerText = question;
+    }
+
+    }
